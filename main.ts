3b69cc2eb8c9cc3a81e6176a5af40005e0130a06
Ubikuiity <pathierfabien@gmail.com>
2022-10-24 23:25:06 +0200

    First version of the plugin. Everything is given is logs. - Implemented the settings menu - Removed all the templates part of the project - Implemented modification date tracking and emoji tracking in files

--- main.ts
+++ main.ts
@@ -1,81 +1,55 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Console } from 'console';
+import { fstat, Stats } from 'fs';
+import { addIcon, App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, SettingTab } from 'obsidian';
+import { text } from 'stream/consumers';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+let fsp = require('fs/promises')
+
+interface Settings {
+	newsLogo: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: Settings = {
+	newsLogo: '❄'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class NewsPlugin extends Plugin {
+	settings: Settings;
 
+	// Executed when Plugin starts (Obsidian opens)
 	async onload() {
 		await this.loadSettings();
+		const newsLogo = this.settings.newsLogo;
+
+		// This calls the menu where the user can change the parameters
+		this.addSettingTab(new SettingsMenu(this.app, this));
+
+		// Add custom ribbon to library
+		addIcon('New', `<text x="50" y="70" font-size="45" text-anchor="middle" fill="white">NEW</text>`);
+		const basePath = (this.app.vault.adapter as any).basePath;
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('New', 'Sample Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			new Notice(`The news logo is : ${newsLogo}`);
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		// iterates over all files, get their modification dates and check if they contains the new icon
+		const files = this.app.vault.getMarkdownFiles();
+		for (let i = 0; i < files.length; i++){
+			let fPath = files[i].path;
+			const pathOfFile = basePath + `\\` + fPath
+			const modDate = await getModificationDates(pathOfFile);
+			console.log(`File was modified on : ${modDate}`);
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+			const content = await this.app.vault.cachedRead(files[i]);
+			
+			if (content.search(newsLogo) != -1){
+				console.log(`Found new file : ${fPath}`)
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		}
 	}
 
 	onunload() {
@@ -91,47 +65,36 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+// Function that gets last modification date of file
+async function getModificationDates(pathToExplore: string){
+	const stats = await fsp.stat(pathToExplore);
+	return stats.mtime;
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+// This creates the menu where the user can change the parameters
+export class SettingsMenu extends PluginSettingTab{
+	plugin: NewsPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: NewsPlugin){
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
-	display(): void {
-		const {containerEl} = this;
-
+	display() {
+		let {containerEl} = this;
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("NewsLogo")
+			.setDesc("Logo utilisé pour marquer les News")
+			.addText((text) => 
+				text
+					.setPlaceholder("Logo des News")
+					.setValue(this.plugin.settings.newsLogo)
+					.onChange(async (value) => {
+						this.plugin.settings.newsLogo = value;
+						await this.plugin.saveSettings();
+					})
+			)
 	}
 }
