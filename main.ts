13df651a106d29c3d51d7a96cc17faea9d945a1d
Ubikuiity <pathierfabien@gmail.com>
2022-10-26 22:02:33 +0200

    Correction of 'Adding display News Menu'
    Updating the files main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,10 @@
 import { Console } from 'console';
-import { fstat, Stats } from 'fs';
+import { fstat, FSWatcher, Stats } from 'fs';
 import { addIcon, App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, SettingTab } from 'obsidian';
 import { text } from 'stream/consumers';
-
-// Remember to rename these classes and interfaces!
-
-let fsp = require('fs/promises')
+import { ExampleView, VIEW_TYPE_EXAMPLE } from "./view";
+import * as fsp from 'fs/promises';
+// let fsp = require('fs/promises')
 
 interface Settings {
 	newsLogo: string;
@@ -20,8 +19,15 @@ export default class NewsPlugin extends Plugin {
 
 	// Executed when Plugin starts (Obsidian opens)
 	async onload() {
+		// Handle settingsof plugin
 		await this.loadSettings();
 		const newsLogo = this.settings.newsLogo;
+
+		// Register View
+		this.registerView(
+			VIEW_TYPE_EXAMPLE,
+			(leaf) => new ExampleView(leaf)
+		  );
 
 		// This calls the menu where the user can change the parameters
 		this.addSettingTab(new SettingsMenu(this.app, this));
@@ -30,8 +36,13 @@ export default class NewsPlugin extends Plugin {
 		addIcon('New', `<text x="50" y="70" font-size="45" text-anchor="middle" fill="white">NEW</text>`);
 		const basePath = (this.app.vault.adapter as any).basePath;
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('New', 'Sample Plugin', (evt: MouseEvent) => {
+		// This part creates the ribbon Icon to display the news view
+		const ribbonIconEl2 = this.addRibbonIcon('dice', 'News Menu', (evt: MouseEvent) => {
+			this.activateMenu();
+		})
+
+		// This creates the news Icon
+		const ribbonIconEl = this.addRibbonIcon('New', 'News Icon', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			new Notice(`The news logo is : ${newsLogo}`);
 		});
@@ -39,12 +50,12 @@ export default class NewsPlugin extends Plugin {
 		// iterates over all files, get their modification dates and check if they contains the new icon
 		const files = this.app.vault.getMarkdownFiles();
 		for (let i = 0; i < files.length; i++){
-			let fPath = files[i].path;
-			const pathOfFile = basePath + `\\` + fPath
-			const modDate = await getModificationDates(pathOfFile);
+			let fPath: string = files[i].path;
+			const pathOfFile :string = basePath + `\\` + fPath
+			const modDate: Date = await getModificationDate(pathOfFile);
 			console.log(`File was modified on : ${modDate}`);
 
-			const content = await this.app.vault.cachedRead(files[i]);
+			const content :string = await this.app.vault.cachedRead(files[i]);
 			
 			if (content.search(newsLogo) != -1){
 				console.log(`Found new file : ${fPath}`)
@@ -52,8 +63,25 @@ export default class NewsPlugin extends Plugin {
 		}
 	}
 
-	onunload() {
+	async activateMenu() {
+		// this is to avoid having multiple leaves in the right pane all the same
+		this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE);
+		
+		// this create the leaf on the right pane
+		await this.app.workspace.getRightLeaf(false).setViewState({
+			type: VIEW_TYPE_EXAMPLE,
+			active: true,
+		});
 
+		// Reveals the Right Leaf, because it needs user to show it otherwise
+		this.app.workspace.revealLeaf(
+			this.app.workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE)[0]
+		);
+	}
+
+	onunload() {
+		// this avoid leaving the leaf if the plugin is removed
+		this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE);
 	}
 
 	async loadSettings() {
@@ -66,8 +94,8 @@ export default class NewsPlugin extends Plugin {
 }
 
 // Function that gets last modification date of file
-async function getModificationDates(pathToExplore: string){
-	const stats = await fsp.stat(pathToExplore);
+async function getModificationDate(pathToExplore: string): Promise<Date>{
+	const stats: Stats = await fsp.stat(pathToExplore);
 	return stats.mtime;
 }
 
